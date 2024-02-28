503b444f2d27516775eeb07eac6dd962fe6ae6de
Sv443 <contact@sv443.net>
2024-02-29 00:06:38 +0100

    feat: initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,30 @@
+module.exports = {
+  ignorePatterns: ["dist/"],
+  env: {
+    browser: true,
+    es2023: true,
+    node: true,
+  },
+  extends: ["plugin:react/recommended", "plugin:prettier/recommended"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react", "react-hooks", "@typescript-eslint"],
+  rules: {
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "error",
+    "react/react-in-jsx-scope": "off",
+    "react/prop-types": "off",
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  overrides: [{ files: ["*.ts", "*.tsx"] }],
+};
