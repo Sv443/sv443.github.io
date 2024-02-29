e35f06a9c224713dbdb3bed6d59da85ae45e7352
Sv443 <contact@sv443.net>
2024-02-29 18:07:31 +0100

    feat: dotfiles & gh actions

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,7 +8,7 @@ module.exports = {
   extends: [
     "plugin:react/recommended",
     "plugin:prettier/recommended",
-    "plugin:storybook/recommended"
+    "plugin:storybook/recommended",
   ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
@@ -20,6 +20,35 @@ module.exports = {
   },
   plugins: ["react", "react-hooks", "@typescript-eslint"],
   rules: {
+    "no-unreachable": "off",
+    quotes: ["error", "double"],
+    semi: ["error", "always"],
+    "eol-last": ["error", "always"],
+    "no-async-promise-executor": "off",
+    "no-cond-assign": "off",
+    indent: [
+      "error",
+      2,
+      { ignoredNodes: ["VariableDeclaration[declarations.length=0]"] },
+    ],
+    "@typescript-eslint/no-non-null-assertion": "off",
+    "@typescript-eslint/no-unused-vars": [
+      "warn",
+      { ignoreRestSiblings: true, argsIgnorePattern: "^_" },
+    ],
+    "@typescript-eslint/ban-ts-comment": "off",
+    "@typescript-eslint/ban-types": [
+      "error",
+      {
+        types: {
+          "{}": false,
+        },
+        extendDefaults: true,
+      },
+    ],
+    "@typescript-eslint/no-explicit-any": "off",
+    "comma-dangle": ["error", "only-multiline"],
+    "no-misleading-character-class": "off",
     "react-hooks/rules-of-hooks": "error",
     "react-hooks/exhaustive-deps": "error",
     "react/react-in-jsx-scope": "off",
